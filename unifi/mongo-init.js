11783dd18597408933c80f1ba0fe50d4b886a4b1
William Tafe <william.tafe@jll.com>
2026-05-29 15:38:13 -0400

    update password retrieval method in mongo-init.js to use _getEnv for environment variable

--- unifi/mongo-init.js
+++ unifi/mongo-init.js
@@ -2,7 +2,7 @@ db = db.getSiblingDB("unifi");
 
 db.createUser({
   user: "unifi",
-  pwd: process.env.MONGO_PASS,
+  pwd: _getEnv("MONGO_PASS"),
   roles: [
     { role: "dbOwner", db: "unifi" },
     { role: "dbOwner", db: "unifi_stat" }
